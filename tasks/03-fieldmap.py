279cb73c87552eb4be4517dd7cbc63cd211f8cde
mathieudesro <mathieudesro@hotmail.com>
2014-12-09 10:27:03 -0500

    improve fieldmap tasks

--- tasks/03-fieldmap.py
+++ tasks/03-fieldmap.py
@@ -1,4 +1,5 @@
 from generic.generictask import GenericTask
+from modules import util
 import scipy.ndimage
 import nibabel
 import numpy
@@ -12,7 +13,6 @@ class Fieldmap(GenericTask):
 
     def __init__(self, subject):
         GenericTask.__init__(self, subject, "preparation", "parcellation")
-
 
     def implement(self):
         """Placeholder for the business logic implementation
@@ -39,8 +39,12 @@ class Fieldmap(GenericTask):
         self.__coregisterFieldmapToAnat(mag, anatFreesurfer)
         self.__invertFieldmapToAnat()
         self.__interpolateAnatMaskToFieldmap(anat, mag, mask)
-        self.__computeFieldmap(phaseRescale, mask)
+        fieldmap = self.__computeFieldmap(phaseRescale, mask)
 
+	self.__simulateLossyMap(fieldmap, mask)
+
+	magBrain = self.__computeMap(mag, mask, 'brain')
+	self.__computeMap(magBrain, mask, 'lossy')
 
     #@TODO change rebase name
     def __rescaleFieldMap(self, source):
@@ -51,45 +55,38 @@ class Fieldmap(GenericTask):
         except ValueError:
             deltaTE = 0.00246
 
-        factor = (math.pi)/()
-        cmd = "fslmath {} -mul {} -div {} {} -odt float".format(source, math.pi, 4096 *deltaTE, target)
+        cmd = "fslmaths {} -mul {} -div {} {} -odt float".format(source, math.pi, 4096 *deltaTE, target)
         self.launchCommand(cmd)
 
         return target
 
 
     def __createSegmentationMask(self, source):
-        """
-        Developped by basile
+	target = self.getTarget(source, 'mask')
 
-        Args:
-            source: a freesurfer aparc+aseg files
-        """
-        target = self.getTarget(source, "mask")
-
-        niis = [nibabel.load(f) for f in source]
-        data = [n.get_data() for n in niis]
-        mask = numpy.logical_or(data[0]>.5,data[1]>.5)
-        numpy.logical_or(data[2]>.9,mask,mask)
-        numpy.logical_or(numpy.logical_and(data[0]+data[1]>.2,data[2]>.5),mask,mask)
-        scipy.ndimage.binary_fill_holes(mask,None,mask)
-        nibabel.save(nibabel.Nifti1Image(mask.astype(numpy.uint8),niis[0].get_affine()), target)
-        del niis, data, mask
-        return target
+	nii = nibabel.load(source)
+	op = ((numpy.mgrid[:5,:5,:5]-2.0)**2).sum(0)<=4
+	mask = scipy.ndimage.binary_closing(nii.get_data()>0, op, iterations=2)
+	scipy.ndimage.binary_fill_holes(mask, output=mask)
+	nibabel.save(nibabel.Nifti1Image(mask.astype(numpy.uint8), nii.get_affine()), target)
+	del nii, mask, op
+	return target
 
 
 
     def __coregisterFieldmapToAnat(self, source, reference):
 
         target = self.getTarget(source, "flirt")
-        cmd = "flirt -in {} -ref {} -out {} -omat {} -cost{} -searchcost {} -dof {}"\
+
+        cmd = "flirt -in {} -ref {} -out {} -omat {} -cost {} -searchcost {} -dof {} "\
             .format(source, reference , target,
-                    self.get("matrix"), self.get("cost"), self.get("searchcost"), self.get("dof"),
-                    self.get("searchrx"), self.get("searchry"), self.get("searchrz"))
+                    self.get("matrix"), self.get("cost"), self.get("searchcost"), self.get("dof"))
 
         if self.getBoolean("usesqform"):
             cmd += "-usesqform "
 
+	#uncompress resulting file
+	util.gunzip("{}.gz".format(target))
         self.launchCommand(cmd)
 
 
@@ -117,12 +114,10 @@ class Fieldmap(GenericTask):
         self.launchCommand(cmd)
         return target
 
-    def __computeFiledmap(self, source, mask):
 
-        source = self.getImage(self.workingDir, 'anat', 'flirt')
+    def __computeFieldmap(self, source, mask):
 
         target = self.getTarget(source, 'reg')
-
 
         # compute the fieldmap
         #--asym=-0.0024600000 echo Time 1 - echoTime 2
@@ -137,25 +132,47 @@ class Fieldmap(GenericTask):
         self.launchCommand(cmd)
 
 
+    def __simulateLossyMap(source, mask):
 
+	## the following step simulate a lossy distorted image from fieldmap magnitude file to improve registration with EPI
+	# compute signal loss in fieldmap space
+	#sigloss --te=0.094000 -i /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/dwi_fieldmap/_subject_HC_AM32_1/make_fieldmap/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_field_reg.nii.gz -m /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/dwi_fieldmap/_subject_HC_AM32_1/warp_t1_mask/HC_AM32_1_mask_crop_flirt.nii.gz -s /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/epi_correction/_subject_HC_AM32_1/signal_loss/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_field_reg_sigloss.nii.gz
 
-        """
+	target = self.getTarget(source, 'sigloss')
+	cmd = "sigloss --te={} -i {} -m {} -s".format(self.get('echo_time'), source, mask, target)
 
-        ## the following step simulate a lossy distorted image from fieldmap magnitude file to improve registration with EPI
-        # compute signal loss in fieldmap space
-        sigloss --te=0.094000 -i /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/dwi_fieldmap/_subject_HC_AM32_1/make_fieldmap/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_field_reg.nii.gz -m /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/dwi_fieldmap/_subject_HC_AM32_1/warp_t1_mask/HC_AM32_1_mask_crop_flirt.nii.gz -s /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/epi_correction/_subject_HC_AM32_1/signal_loss/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_field_reg_sigloss.nii.gz
+	self.launchCommand(cmd)
 
+    #@TODO find another prefix instead of mask and suffix brain
+    def __computeMap(source, mask, prefix):
 
         # mask the fieldmap magnitude file
-        fslmaths /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/_subject_HC_AM32_1/convert_fieldmap_dwi_dicom/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_mag.nii.gz -mul /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/dwi_fieldmap/_subject_HC_AM32_1/warp_t1_mask/HC_AM32_1_mask_crop_flirt.nii.gz /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/dwi_fieldmap/_subject_HC_AM32_1/mask_mag/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_mag_brain.nii
+        #fslmaths /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/_subject_HC_AM32_1/convert_fieldmap_dwi_dicom/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_mag.nii.gz -mul /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/dwi_fieldmap/_subject_HC_AM32_1/warp_t1_mask/HC_AM32_1_mask_crop_flirt.nii.gz /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/dwi_fieldmap/_subject_HC_AM32_1/mask_mag/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_mag_brain.nii
 
         # compute the fieldmap magnitude file with signal loss
-        fslmaths /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/dwi_fieldmap/_subject_HC_AM32_1/mask_mag/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_mag_brain.nii -mul /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/epi_correction/_subject_HC_AM32_1/signal_loss/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_field_reg_sigloss.nii.gz /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/epi_correction/_subject_HC_AM32_1/fieldmap_mag_lossy/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_mag_brain_lossy.nii
+        #fslmaths /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/dwi_fieldmap/_subject_HC_AM32_1/mask_mag/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_mag_brain.nii -mul /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/epi_correction/_subject_HC_AM32_1/signal_loss/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_field_reg_sigloss.nii.gz /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/epi_correction/_subject_HC_AM32_1/fieldmap_mag_lossy/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_mag_brain_lossy.nii
 
+	target = self.getTarget(source, prefix)
+
+	cmd = "fslmaths {} -mul {} ".format(source, mask, target)
+	self.launchCommand(cmd)
+
+	
+    def __computeForwardDistorsion(source, lossyImage, mask):
         #--dwell=Effective echo spacing
         #--unwarpdir=y < piege a la con
         # compute forward distortion on lossy fielmap magnitude file
-        fugue --dwell=0.0006900000 --loadfmap=/media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/dwi_fieldmap/_subject_HC_AM32_1/make_fieldmap/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_field_reg.nii.gz --in=/media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/epi_correction/_subject_HC_AM32_1/fieldmap_mag_lossy/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_mag_brain_lossy.nii --mask=/media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/dwi_fieldmap/_subject_HC_AM32_1/warp_t1_mask/HC_AM32_1_mask_crop_flirt.nii.gz --nokspace --unwarpdir=y --warp=fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_mag_brain_lossy_warped.nii.gz
+        #fugue --dwell=0.0006900000 --loadfmap=/media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/dwi_fieldmap/_subject_HC_AM32_1/make_fieldmap/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_field_reg.nii.gz --in=/media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/epi_correction/_subject_HC_AM32_1/fieldmap_mag_lossy/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_mag_brain_lossy.nii --mask=/media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/dwi_fieldmap/_subject_HC_AM32_1/warp_t1_mask/HC_AM32_1_mask_crop_flirt.nii.gz --nokspace --unwarpdir=y --warp=fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_mag_brain_lossy_warped.nii.gz
+
+
+	target = self.getTarget(source, 'warped')
+	
+	cmd = "fugue --dwell={} --loadfmap={} --in={} --mask={}  --nokspace --unwarpdir={} --warp={} ".format(self.get('dwell_time'), source, lossyImage, self.get('unwarpdir'), target )
+
+	self.launchCommand(cmd)
+
+        """
+
 
         # coregister the epi with lossy distorted fieldmap magnitude
         flirt -in /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/_subject_HC_AM32_1/dwi_convert/20120913_131105DTIb0Saads007a1001.nii -ref /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/epi_correction/_subject_HC_AM32_1/fm_voxelshiftmap/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_mag_brain_lossy_warped.nii.gz -out 20120913_131105DTIb0Saads007a1001_flirt.nii.gz -omat epi_to_b0fm -cost normmi -searchcost normmi -dof 6 -interp trilinear -refweight /media/77f462a2-7290-437d-8209-c1e673ed635a/analysis/cardio_pd/epi_correction/_subject_HC_AM32_1/signal_loss/fieldmap_dwi_CARDIO_HC_C_AM32_1_20120913_field_reg_sigloss.nii.gz -searchrx -5 5 -searchry -5 5 -searchrz -5 5
