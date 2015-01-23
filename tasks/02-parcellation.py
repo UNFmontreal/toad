dedc51fd771f77d161a09c071782603d924a06c9
mathieudesro <mathieudesro@hotmail.com>
2015-01-23 12:42:55 -0500

    update parcellation tasks

--- tasks/02-parcellation.py
+++ tasks/02-parcellation.py
@@ -16,35 +16,51 @@ class Parcellation(GenericTask):
     def implement(self):
 
 
-        images = {'aparc_aseg':self.getImage(self.dependDir, 'aparc_aseg'),
-                    'freesurfer_anat':self.getImage(self.dependDir, 'freesurfer_anat'),
-                    'rh_ribbon':self.getImage(self.dependDir, 'rh_ribbon'),
-                    'lh_ribbon':self.getImage(self.dependDir, 'lh_ribbon'),
-                    'brodmann':self.getImage(self.dependDir, 'brodmann')}
-
-        natives = {'freesurfer_anat': "{}/*/mri/T1.mgz",
-                     'aparc_aseg': "{}/*/mri/aparc+aseg.mgz",
-                     'rh_ribbon': "{}/*/mri/rh.ribbon.mgz",
-                     'lh_ribbon': "{}/*/mri/lh.ribbon.mgz"}
+        images = {'aparc_aseg': self.getImage(self.dependDir, 'aparc_aseg'),
+                    'freesurfer_anat': self.getImage(self.dependDir, 'freesurfer_anat'),
+                    'rh_ribbon': self.getImage(self.dependDir, 'rh_ribbon'),
+                    'lh_ribbon': self.getImage(self.dependDir, 'lh_ribbon'),
+                    'brodmann': self.getImage(self.dependDir, 'brodmann')}
 
 
-        self.__submitReconAllIfNeeded(natives, images)
         unlinkedImages = self.__linkExistingImage(images)
-        self.__convertReconAllImageIntoNifti(natives, unlinkedImages)
 
-        #create the brodmann image if not existing
-        if not images['brodmann']:
-            self.__createBrodmannAreaFromMricronTemplate()
+        if len(unlinkedImages) > 0:
+            #If some images are missing submit recon-all if needed
+            self.__submitReconAllIfNeeded()
+
+
+        if unlinkedImages.has_key('brodmann'):
+            self.__createBrodmannAreaImageFromMricronTemplate()
+            del(unlinkedImages['brodmann'])
+
+        self.__convertFeesurferImageIntoNifti(unlinkedImages)
 
         if self.getBoolean('cleanup'):
             self.__cleanup()
 
-    def __convertReconAllImageIntoNifti(self, natives, images):
-        #convert images of interest into nifti
-        for key, value in images.iteritems():
-            images = glob.glob(natives[key].format(self.workingDir))
-            if len(images) > 0:
-                self.__convertAndRestride(images.pop(), os.path.join(self.workingDir, self.get(key)))
+
+    def __submitReconAllIfNeeded(self):
+        prerequisites = ["{}/*/mri/T1.mgz",
+                        "{}/*/mri/aparc+aseg.mgz",
+                        "{}/*/mri/rh.ribbon.mgz",
+                        "{}/*/mri/lh.ribbon.mgz",
+                         "{}/*/mri/norm.mgz"]
+
+        for prerequisite in prerequisites:
+            images = glob.glob(prerequisite.format(self.workingDir))
+            if len(images) == 0:
+                missing = True
+            else:
+                missing = False
+
+            if missing:
+                #recon-all need to be resubmit
+                self.info("Set SUBJECTS_DIR to {}".format(self.workingDir))
+                os.environ["SUBJECTS_DIR"] = self.workingDir
+                anat = self.getImage(self.dependDir, 'anat')
+                self.__reconAll(anat)
+
 
 
     def __linkExistingImage(self, images):
@@ -59,38 +75,31 @@ class Parcellation(GenericTask):
         return unlinkedImages
 
 
-    def __createBrodmannAreaFromMricronTemplate(self):
+    def __convertFeesurferImageIntoNifti(self, images):
+        natives = {'freesurfer_anat': "{}/*/mri/T1.mgz",
+                     'aparc_aseg': "{}/*/mri/aparc+aseg.mgz",
+                     'rh_ribbon': "{}/*/mri/rh.ribbon.mgz",
+                     'lh_ribbon': "{}/*/mri/lh.ribbon.mgz"}
+        for key, value in images.iteritems():
+            images = glob.glob(natives[key].format(self.workingDir))
+            if len(images) > 0:
+                self.__convertAndRestride(images.pop(), os.path.join(self.workingDir, self.get(key)))
+
+
+    def __createBrodmannAreaImageFromMricronTemplate(self):
 
         brodmannTemplate = os.path.join(self.toadDir, self.get("templates_brodmann"))
         target = self.get("brodmann")
 
         #@TODO remove all trace of mgz file
-        cmd = "mri_vol2vol --mov {} --targ $SUBJECTS_DIR/fsaverage/mri/T1.mgz" \
+        cmd = "mri_vol2vol --mov {} --targ $FREESURFER_HOME/subjects/fsaverage/mri/T1.mgz" \
               " --o brodmann_fsaverage.mgz --regheader --interp nearest".format(brodmannTemplate)
         self.launchCommand(cmd)
 
-        cmd =  "mri_vol2vol --mov $SUBJECTS_DIR/freesurfer/mri/norm.mgz --s freesurfer --targ brodmann_fsaverage.mgz" \
-               " --m3z talairach.m3z --o {} --interp nearest --inv-morph".format(target)
+        cmd =  "mri_vol2vol --mov {}/mri/norm.mgz --s freesurfer --targ brodmann_fsaverage.mgz" \
+               " --m3z talairach.m3z --o {} --interp nearest --inv-morph".format(self.id, target)
         self.launchCommand(cmd)
         return self.__convertAndRestride(target, target)
-
-
-    def __submitReconAllIfNeeded(self, natives, images):
-        if not (images['aparc_aseg'] and images['freesurfer_anat'] and images['rh_ribbon'] and images['lh_ribbon']):
-
-            for key, value in natives.iteritems():
-                images = glob.glob(value.format(self.workingDir))
-                if len(images) == 0:
-                    missing = True
-                else:
-                    missing = False
-
-            if missing:
-                #recon-all need to be resubmit
-                self.info("Set SUBJECTS_DIR to {}".format(self.workingDir))
-                os.environ["SUBJECTS_DIR"] = self.workingDir
-                anat = self.getImage(self.dependDir, 'anat')
-                self.__reconAll(anat)
 
 
     def __reconAll(self, source):
