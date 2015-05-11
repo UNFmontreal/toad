cec7826bbf64e5d58a528e388c1e69fd7926aa26
mathieudesro <mathieudesro@hotmail.com>
2015-05-11 13:49:58 -0400

    first draft 15-tractographydipy.py

--- tasks/15-tractographydipy.py
+++ tasks/15-tractographydipy.py
@@ -6,6 +6,8 @@ from dipy.tracking.local import ActTissueClassifier, LocalTracking
 from dipy.direction import DeterministicMaximumDirectionGetter
 from dipy.data import default_sphere
 from dipy.io.trackvis import save_trk
+import matplotlib
+matplotlib.use('Agg')
 
 __author__ = 'desmat'
 
@@ -20,13 +22,11 @@ class TractographyDipy(GenericTask):
 
         act = self.getImage(self.maskingDir, "aparc_aseg", ['resample', 'act'])
         csd = self.getImage(self.hardimrtrixDir, "dwi", 'csd')
-
+        """
         actImage = nibabel.load(act)
         csdImage = nibabel.load(csd)
 
-        actImage = actImage.get_data()
-        print actImage.shape
-        self.quit()
+        actData = actImage.get_data()
 
         #gm = self.buildName(act, "gm")
         #kernel = self.buildName(act, "kernel")
@@ -47,43 +47,66 @@ class TractographyDipy(GenericTask):
         #excludeImage = nibabel.load(csf)
         #csdImage = nibabel.load(csd)
 
-
-        #includeData  = includeImage.get_data()
-        #excludeData = excludeImage.get_data()
+        #concatenate grey and kernel information from act
+        includeData  = numpy.logical_or(actData[:,:,:,0], actData[:,:,:,1])
+        includeImage = nibabel.Nifti1Image(includeData.astype(numpy.float32), actImage.get_affine())
+        nibabel.save(includeImage, self.buildName(act, "include"))
+        excludeData = actData[:,:,:,3] #csf
         csdData = csdImage.get_data()
 
         classifier = ActTissueClassifier(includeData, excludeData)
-        classifierImage = nibabel.Nifti1Image(classifier.astype(numpy.float32), includeData.get_affine())
-        nibabel.save(classifierImage, self.buildName(act, "classifier"))
-
-
-        deterministicGetter = DeterministicMaximumDirectionGetter.from_shcoeff(csdImage.shm_coeff,
+	
+	figure = matplotlib.figure()
+	matplotlib.subplot(121)
+	matplotlib.xticks([])
+	matplotlib.yticks([])
+	matplotlib.imshow(includeData[:, :, actData.shape[2] / 2].T, cmap='gray', origin='lower',
+		   interpolation='nearest')
+	matplotlib.subplot(122)
+	matplotlib.xticks([])
+	matplotlib.yticks([])
+	matplotlib.imshow(excludeData[:, :, actData.shape[2] / 2].T, cmap='gray', origin='lower',
+		   interpolation='nearest')
+	figure.tight_layout()
+	figure.savefig('act_maps.png')
+        
+        print "includeImage.get_affine()=", includeImage.get_affine()
+        print "csdImage.get_affine()=",csdImage.get_affine()
+        deterministicGetter = DeterministicMaximumDirectionGetter.from_shcoeff(csdData,
                                                       max_angle=30.,
                                                       sphere=default_sphere)
 
 
         validStreamlinesActClassifier = LocalTracking(deterministicGetter,
                                                          classifier,
-                                                         wm,
-                                                         includeData.get_affine(),
+                                                         actData[:,:,:,2], #white matter
+                                                         csdImage.get_affine(),
                                                          step_size=.5,
                                                          return_all=False)
 
+
+     	print validStreamlinesActClassifier.maxlen
+        print validStreamlinesActClassifier.direction_getter
+        print validStreamlinesActClassifier.tissue_classifier
+        print validStreamlinesActClassifier.step_size
+        print validStreamlinesActClassifier.fixed
+        print validStreamlinesActClassifier.max_cross
+        print validStreamlinesActClassifier._voxel_size
         #@TODO find a souton for label
         save_trk("deterministic_act_classifier_valid.trk",
                  validStreamlinesActClassifier,
-                 includeData.get_affine(),
+                 csdImage.get_affine(),
                  includeData.shape)
 
-
+        """
     def isIgnore(self):
         return self.get("ignore").lower() in "true"
 
 
     def meetRequirement(self):
-        return Images(self.getImage(self.maskingDir, "aparc_aseg", ["resample", "act"]), 'resampled anatomically constrained tractography',
-                      self.getImage(self.hardimrtrixDir, "dwi", 'csd'), 'constrained spherical deconvolution')\
-            .isSomeImagesMissing()
+        return Images((self.getImage(self.maskingDir, "aparc_aseg", ["resample", "act"]), 'resampled anatomically constrained tractography'),
+                      (self.getImage(self.hardimrtrixDir, "dwi", 'csd'), 'constrained spherical deconvolution'))\
+            .isAllImagesExists()
 
     def isDirty(self):
         return True
