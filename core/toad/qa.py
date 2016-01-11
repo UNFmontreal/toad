d65143e4013b9728193af2bb3e9b98f1f5830586
Christophe Bedetti <christophe.bedetti@criugm.qc.ca>
2016-01-11 16:08:54 -0500

    Add qautil wrappers in qa

--- core/toad/qa.py
+++ core/toad/qa.py
@@ -12,29 +12,136 @@ __credits__ = ["Christophe Bedetti", "Mathieu Desrosiers"]
 class Qa(object):
 
     def __init__(self):
-        self.imageFormat = 'jpg'
+        self.qaImageFormat = 'jpg'
+        self.qaImagesDir = os.path.join(
+                self.qaDir, self.config.get('qa', 'images_dir'), self.getName())
+        self.qaMainTemplate = os.path.join(
+                self.toadDir, 'templates', 'files', 'qa.main.tpl')
+        self.qaIndex = os.path.join(
+                self.qaDir, '{}.html'.format(self.getName()))
 
 
-    def plot3dVolume(self, source):
+    def plot3dVolume(
+            self, source, edges=None, segOverlay=None, fov=None, postfix=None):
         """
         Wrapper of the class Plot3dVolume of qautil
         """
-        target = self.buildName(source, None, ext=self.imageFormat)
-        qaPlot = qautil.Plot3dVolume(source)
+        target = self.buildName(source, postfix, ext=self.qaImageFormat)
+        qaPlot = qautil.Plot3dVolume(source, fov=fov)
+        if segOverlay != None:
+            lut = self.config.get('template', 'freesurfer_lut')
+            lutFiles = os.path.join(
+                    self.toadDir, 'templates', 'lookup_tables', lut)
+            qaPlot.setSegOverlay(segOverlay, lutFiles)
+            target = self.buildName(segOverlay, None, ext=self.qaImageFormat)
+        if edges !=None:
+            qaPlot.setEdges(edges)
+            target = self.buildName(edges, None, ext=self.qaImageFormat)
         qaPlot.save(target)
-        #lut = self.config.get('template', 'freesurfer_lut')
-        #lutFiles = os.path.join(self.toadDir, 'templates', 'lookup_tables', lut)
         return target
 
 
-    def plot4dVolume(self, source):
+    def plot4dVolume(self, source, fov=None):
         """
         Wrapper of the class Plot4dVolume of qautil
         """
         target = self.buildName(source, None, ext='gif')
-        qaPlot = qautil.Plot4dVolume(source)
+        qaPlot = qautil.Plot4dVolume(source, fov=fov)
         qaPlot.save(target)
         return target
+
+
+    def compare4dVolumes(self, source1, source2, fov=None):
+        """
+        Wrapper of the class Plot4dVolume of qautil
+        """
+        target = self.buildName(source1, 'compare', ext='gif')
+        qaPlot = qautil.Plot4dVolume(source1, source2=source2, fov=fov)
+        qaPlot.save(target)
+        return target
+
+
+    def plotMovement(self, parametersFile, basename):
+        """
+        """
+        targetTranslations = self.buildName(
+                basename, 'translations', ext=self.qaImageFormat)
+        targetRotations = self.buildName(
+                basename, 'rotations', ext=self.qaImageFormat)
+        qautil.plotMovement(
+                parametersFile, targetTranslations, targetRotations)
+        return targetTranslations, targetRotations
+
+
+    def plotVectors(self, bvecsFile, bvecsCorrected, basename):
+        """
+        """
+        target = self.buildName(basename, 'vectors', ext='gif')
+        qautil.plotVectors(bvecsFile, bvecsCorrected, target)
+        return target
+
+
+    def plotSigma(self, sigma, basename):
+        """
+        """
+        target = self.buildName(basename, 'sigma', ext=self.qaImageFormat)
+        qautil.plotSigma(sigma, target)
+        return target
+
+
+    def noiseAnalysis(self, source, maskNoise, maskCc):
+        """
+        """
+        targetSnr = self.buildName(source, 'snr', ext=self.qaImageFormat)
+        targetHist = self.buildName(source, 'hist', ext=self.qaImageFormat)
+        qautil.noiseAnalysis(source, maskNoise, maskCc, targetSnr, targetHist)
+        return targetSnr, targetHist
+
+
+    def plotReconstruction(self, data, mask, cc, model, basename):
+        """
+        """
+        target = self.buildName(basename, model, ext=self.qaImageFormat)
+        qautil.plotReconstruction(data, mask, cc, target, model)
+        return target
+
+
+    def createVtkPng(self, source, anatomical, roi):
+        """
+        """
+        target = source.replace(".trk", self.qaImageFormat)
+        qautil.createVtkPng(source, anatomical, roi, target)
+        return target
+
+
+    def updateQaMenu(self):
+        """
+        """
+        taskName = self.getName()
+
+        #Updating menu
+        menuFile = os.path.join(self.qaDir, 'menu.html')
+        menuLinkTemplate = '<li><a id="{0}" href="{0}.html" target="_top">{0}</a></li>\n'
+
+        with open(menuFile, 'r') as f:
+            lines = f.readlines()
+        if not any(taskName in line for line in lines):
+            lines.insert(-1, menuLinkTemplate.format(taskName))
+        with open(menuFile, 'w') as f:
+            for line in lines:
+                f.write(line)
+
+        #Create temporary html
+        htmlTaskFile = os.path.join(self.qaDir, "{}.html".format(taskName))
+        message = "Task is being processed. Refresh to check completion."
+        tags = {
+            'subject':self.subject.getName(),
+            'taskInfo':'',
+            'parseHtmlTables':message,
+            'parseVersionTables': '',
+            }
+        htmlCode = self.parseTemplate(tags, self.qaMainTemplate)
+        util.createScript(htmlTaskFile, htmlCode)
 
 
     def createQaReport(self, images):
@@ -42,23 +149,46 @@ class Qa(object):
         Args:
            images : an Images object
         """
-        mainTemplate = os.path.join(self.toadDir, 'templates', 'files', 'qa.main.tpl')
-        tableTemplate = os.path.join(self.toadDir, 'templates', 'files', 'qa.table.tpl')
-        subjectName = self.subject.getName()
+        if not os.path.exists(self.qaImagesDir):
+            os.makedirs(self.qaImagesDir)
+        tableTemplate = os.path.join(
+                self.toadDir, 'templates', 'files', 'qa.table.tpl')
         taskInfo = images.getInformation()
-        imagesDir = os.path.join(self.qaDir, self.config.get('qa', 'images_dir'))
-        versions = minidom.parse(os.path.join(self.logDir, self.get('general','versions_file_name')))
-        tablesCode = ''
-        
+        versions = minidom.parse(os.path.join(
+                self.logDir, self.get('general','versions_file_name')))
+
         print "createQaReport images =", images
+
+        smallViewTags = [
+                '_translations',
+                '_rotations',
+                '_vectors',
+                '_sigma',
+                '_hist',
+                '_snr',
+                '_ellipsoids']
+        tablesCode = ''
         for imageLink, legend in images:
             if imageLink:
                 path, filename =  os.path.split(imageLink)
-                classType = 'large_view'
-                if any(_ in filename for _ in ['_translations', '_rotations', '_vectors', '_sigma', '_hist', '_snr', '_ellipsoids']):
+
+                if any(tag in filename for tag in smallViewTags):
                     classType = 'small_view'
-                shutil.copyfile(imageLink, os.path.join(imagesDir, filename))
-                relativeLink = os.path.join(self.config.get('qa', 'images_dir'), filename)
+                else:
+                    classType = 'large_view'
+
+                # Copy qa image to qa image directory
+                # default: 00-qa/images/<task name>
+                shutil.copyfile(
+                        imageLink,
+                        os.path.join(self.qaImagesDir, filename))
+
+                # Relative link to insert in html code
+                relativeLink = os.path.join(
+                        self.config.get('qa', 'images_dir'),
+                        self.getName(),
+                        filename)
+
                 tags = {
                     'imageLink':relativeLink,
                     'legend':legend,
@@ -70,13 +200,12 @@ class Qa(object):
                 tablesCode += self.parseTemplate(tags, tableTemplate)
 
         tags = {
-            'subject':subjectName,
+            'subject':self.subject.getName(),
             'taskInfo':taskInfo,
             'parseHtmlTables':tablesCode,
             'parseVersionTables': versions.toprettyxml(),
             }
-        htmlCode = self.parseTemplate(tags, mainTemplate)
-        htmlFile = os.path.join(self.qaDir,'{}.html'.format(self.getName()))
-        util.createScript(htmlFile, htmlCode)
+        htmlCode = self.parseTemplate(tags, self.qaMainTemplate)
+        util.createScript(self.qaIndex, htmlCode)
 
 
