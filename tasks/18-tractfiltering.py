ca2434cfbebaa4f17f9fdfbcec0420ff949e8c62
arnaudbore <arnaud.bore@gmail.com>
2016-04-18 14:43:14 -0400

    New task: tract filtering

---
+++ tasks/18-tractfiltering.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+import os
+from core.toad.generictask import GenericTask
+from lib import mriutil, util
+from lib.images import Images
+
+
+class TractFiltering(GenericTask):
+    def __init__(self, subject):
+        GenericTask.__init__(
+            self, subject, 'tensorfsl', 'tensormrtrix', 'tensordipy',
+            'tractquerier', 'qa')
+        self.setCleanupBeforeImplement(False)
+        self.dirty = True
+
+
+    def implement(self):
+
+        rawTractographies = self.workingDir + os.pathsep + 'raw'
+
+        self.__linksTractographies(rawTractographies)
+
+        configRemoveOutliers = self.get('configRemoveOutliers')
+        self.__removeOutliers(configRemoveOutliers, rawTractographies, rawTractographies)
+
+    def __linksTractographies(self, target):
+        source = self.getTractQuerierImage(None, None, 'trk')
+
+        if not os.path.exists(target):
+            os.mkdir(target)
+
+        for src in source:
+            util.symlink(src, self.workingDir)
+
+    def __removeOutliers(self, config, source, target):
+        cmd = "scil_run_tractometry.py --config_file {} {} -v -f ".format(config, source, target)
+        util.launchCommand(cmd)
+
+
+
+
+
+    def __tractQuerier(self, trk, atlas, qryDict, qryFile):
+        target = self.buildName(trk, None, 'trk')
+        cmd = "tract_querier -t {} -a {} -I {} -q {} -o {}"
+        cmd = cmd.format(trk, atlas, qryDict, qryFile, target)
+        self.launchCommand(cmd)
+        return target
+
+
+    def __buildNameTractQuerierOutputs(self):
+        self.queries = [self.getImage('dwi', 'corpus_callosum', 'trk'),
+                        self.getImage('dwi', 'cortico_spinal.left', 'trk'),
+                        self.getImage('dwi', 'cortico_spinal.right', 'trk'),
+                        self.getImage('dwi', 'inferior_fronto_occipital.left', 'trk'),
+                        self.getImage('dwi', 'inferior_fronto_occipital.right', 'trk'),
+                        self.getImage('dwi', 'inferior_longitudinal_fasciculus.left', 'trk'),
+                        self.getImage('dwi', 'inferior_longitudinal_fasciculus.right', 'trk'),
+                        self.getImage('dwi', 'uncinate_fasciculus.left', 'trk'),
+                        self.getImage('dwi', 'uncinate_fasciculus.right', 'trk')]
+
+
+    def isIgnore(self):
+        return self.get("ignore")
+
+
+    def meetRequirement(self):
+        """Validate if all requirements have been met prior to launch the task
+        Returns:
+            True if all requirement are meet, False otherwise
+        """
+
+        images = Images()
+
+        dwi = self.getUpsamplingImage('dwi', 'upsample')
+        nbDirections = mriutil.getNbDirectionsFromDWI(dwi)
+
+        # Load tractography
+        if nbDirections <= 45:
+            postfixTractography = 'tensor_prob'
+        else:
+            postfixTractography = 'hardi_prob'
+
+        Images((self.getTractographymrtrixImage('dwi', postfixTractography, 'trk'),'Tractography file'),
+                (self.__getAtlas(),'Atlas'))
+
+        return images
+
+
+    def isDirty(self):
+        """Validate if this tasks need to be submit during the execution
+        Returns:
+            True if any expected file or resource is missing, False otherwise
+        """
+
+        dwi = self.getUpsamplingImage('dwi', 'upsample')
+        nbDirections = mriutil.getNbDirectionsFromDWI(dwi)
+
+        # Which tractography
+        if nbDirections <= 45:
+            postfixTractography = 'tensor_prob'
+        else:
+            postfixTractography = 'hardi_prob'
+
+        target_queries = self.getBackupImage('queries', None, 'qry')
+        target_dict = self.getBackupImage('tq_dict', None, 'qry')
+
+        if not target_queries and not target_dict:
+            return Images((self.getImage('dwi', postfixTractography+'_corpus_callosum', 'trk'),'CC'),
+                           (self.getImage('dwi', postfixTractography+'_cortico_spinal.left', 'trk'),'CS_left'),
+                           (self.getImage('dwi', postfixTractography+'_cortico_spinal.right', 'trk'),'CS_right'),
+                           (self.getImage('dwi', postfixTractography+'_inferior_fronto_occipital.left', 'trk'),'IFO_left'),
+                           (self.getImage('dwi', postfixTractography+'_inferior_fronto_occipital.right', 'trk'),'IFO_right'),
+                           (self.getImage('dwi', postfixTractography+'_inferior_longitudinal_fasciculus.left', 'trk'),'ILF_left'),
+                           (self.getImage('dwi', postfixTractography+'_inferior_longitudinal_fasciculus.right', 'trk'),'ILF_right'),
+                           (self.getImage('dwi', postfixTractography+'_uncinate_fasciculus.left', 'trk'),'UF_left'),
+                           (self.getImage('dwi', postfixTractography+'_uncinate_fasciculus.right', 'trk'),'UH_right'))
+
+        return True
+
+
+    def qaSupplier(self):
+        """Create and supply images for the report generated by qa task
+
+        """
+        qaImages = Images()
+
+        information = "Warning: due to storage restriction, streamlines were " \
+                      "downsampled. Even if there is no difference in structural " \
+                      "connectivity, you should be careful before computing any " \
+                      "metrics along these streamlines.\n To run toad without this " \
+                      "downsampling, please refer to the documentation."
+        qaImages.setInformation(information)
+
+        if self.defaultQuery:
+            # get images
+            norm = self.getRegistrationImage("norm", "resample")
+            self.__buildNameTractQuerierOutputs()
+            print self.queries
+            # images production
+            tags = (
+                (self.queries[0],
+                 'Corpus Callosum',
+                 95, 60, 40, -80, 0, 160),
+                (self.queries[1],
+                 'Corticospinal tract Left',
+                 95, 80, 40, -90, 0, 160),
+                (self.queries[2],
+                 'Corticospinal tract right',
+                 95, 80, 40, -90, 0, 200),
+                (self.queries[3],
+                 'Inferior Fronto Occipital tract left',
+                 95, 80, 40, -90, 0, 90),
+                (self.queries[4],
+                 'Inferior Fronto Occipital tract right',
+                 95, 80, 40, -90, 0, -90),
+                (self.queries[5],
+                 'inferior Longitudinal Fasciculus left',
+                 95, 80, 40, -90, 0, 90),
+                (self.queries[6],
+                 'Inferior Longitudinal Fasciculus right',
+                 95, 80, 40, -90, 0, -90),
+                (self.queries[7],
+                 'Uncinate Fasciculus left',
+                 95, 80, 40, -90, 0, 90),
+                (self.queries[8],
+                 'Uncinate Fasciculus right',
+                 95, 80, 40, -90, 0, -90))
+
+            for data, description, xSlice, ySlice, zSlice, xRot, yRot, zRot in tags:
+                if data is not None:
+                    imageQa = self.plotTrk(data, norm, None, xSlice, ySlice, zSlice, xRot, yRot, zRot)
+                    qaImages.append((imageQa, description))
+        else:
+            # Add message about QA
+            pass
+
+        return qaImages
