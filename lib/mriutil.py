b6fd0d1fee117067c498d478d4eeb097c5a606de
arnaudbore <arnaud.bore@gmail.com>
2016-04-19 11:22:24 -0400

    setWorkinDir

--- lib/mriutil.py
+++ lib/mriutil.py
@@ -693,13 +693,18 @@ def setWorkingDirTractometry(workingDir, sourceBundles=None, sourceMetrics=None)
     targetBundlesDir = os.path.join(workingDir, bundlesDir)
     targetMetricsDir = os.path.join(workingDir, metricsDir)
 
+
+
+
     if sourceBundles is not None:
+        print sourceBundles
         if not os.path.exists(bundlesDir):
             os.mkdir(bundlesDir)
             for bundle in sourceBundles:
                 util.symlink(bundle, targetBundlesDir)
 
     if sourceMetrics is not None:
+        print sourceMetrics
         if not os.path.exists(metricsDir):
             os.mkdir(metricsDir)
             if type(sourceMetrics) is list:
