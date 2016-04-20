31f61908057773b88192b9ab19ce7b783e2a46e4
arnaudbore <arnaud.bore@gmail.com>
2016-04-20 11:50:01 -0400

    dev symlink function

--- lib/util.py
+++ lib/util.py
@@ -34,7 +34,7 @@ def symlink(source, targetDir, targetName=None):
     if not os.path.isabs(source):  # Get absolute path source
         source = os.path.abspath(source)
 
-    targetDir = targetDir.replace(os.getcwd(), '')
+    #targetDir = targetDir.replace(os.getcwd(), '')
 
     if not os.path.isabs(targetDir):  # Get absolute path targetDir
         targetDir = os.path.abspath(targetDir)
@@ -58,7 +58,7 @@ def symlink(source, targetDir, targetName=None):
     commonPath = os.path.sep + os.path.join(*os.path.commonprefix((sourceSplit, targetSplit))) + os.path.sep
 
     source = source.replace(commonPath, '')  # Get ride of the commonPath
-    target = target.replace(commonPath, '') # Get ride of the commonPath
+    target = target.replace(os.getcwd(), '') # Get ride of the commonPath
 
     deep = target.count(os.path.sep) + 1  # Number of sub_folders
 
