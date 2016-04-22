2221f95d0b2fcb62750f8bd68eb99f36be482ccb
arnaudbore <arnaud.bore@gmail.com>
2016-04-22 10:03:40 -0400

    dev symlink

--- lib/util.py
+++ lib/util.py
@@ -37,6 +37,8 @@ def symlink(source, targetDir, targetName=None):
     if not os.path.isabs(targetDir):  # Get absolute path targetDir
         targetDir = os.path.abspath(targetDir)
 
+    print 'TargetDir : ' + targetDir
+
     if targetName is None:
         targetName = os.path.basename(source)
 
