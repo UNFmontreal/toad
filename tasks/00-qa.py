b3087ca9cfddecefe994434751d04b306d3b160b
Christophe Bedetti <christophe.bedetti@criugm.qc.ca>
2016-05-12 16:58:11 -0400

    Scaffold for methodology

--- tasks/00-qa.py
+++ tasks/00-qa.py
@@ -40,6 +40,37 @@ class QA(GenericTask):
         #Create index.html
         self.createTaskHtml({}, 'index.html')
 
+        #Create metho.html
+        from jinja2 import Environment, FileSystemLoader
+        templateMetho = os.path.join(
+                self.toadDir, 'templates', 'files')
+        jinja2Env = Environment(loader=FileSystemLoader(templateDir), trim_blocks=True)
+        tpl = jinja2Env.get_template('metho.tpl')
+        tags_metho = self.__getTags()
+        htmlCode = tpl.render(**tags_metho)
+        util.createScript('metho.html', htmlCode)
+
+
+    def __getTags(self):
+        tags = {}
+
+        denoisingKeys = ['number_array_coil']
+        for key in denoisingKeys:
+            tags[key] = self.config.get('denoising', key)
+
+        methodologyKeys = [
+                't1_tr', 't1_te', 't1_ti', 't1_flipangle', 't1_fov', 't1_mat',
+                't1_voxelsize', 'dwi_tr', 'dwi_te', 'dwiflipangle',
+                'dwi_voxelsize', 'dwi_numdirections', 'dwi_bvalue']
+        for key in denoisingKeys:
+            tags[key] = self.config.get('methodology', key)
+
+        """
+        , 'correction',
+'fsl_vers', 'fsl_ref', 'denoising', 'tri_ref', 'fdt_ref', 'seg_ref', 'references']
+        """
+        return tags
+
 
     def meetRequirement(self, result=True):
         """
