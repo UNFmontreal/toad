9f89bc26bc67343dcb4fdd8f844ec7fe2576f6c1
Christophe Bedetti <christophe.bedetti@criugm.qc.ca>
2016-05-13 10:59:19 -0400

    Implementation of methodology

--- tasks/00-qa.py
+++ tasks/00-qa.py
@@ -38,11 +38,13 @@ class QA(GenericTask):
             util.copy(fileLink, self.workingDir, fileName)
 
         #Create index.html
-        self.createTaskHtml({}, 'index.html')
+        self.createTaskHtml(
+                {'parseHtmlTables':'<a href="./metho.html">Methodology example</a>'},
+                'index.html')
 
         #Create metho.html
         from jinja2 import Environment, FileSystemLoader
-        templateMetho = os.path.join(
+        templateDir = os.path.join(
                 self.toadDir, 'templates', 'files')
         jinja2Env = Environment(loader=FileSystemLoader(templateDir), trim_blocks=True)
         tpl = jinja2Env.get_template('metho.tpl')
@@ -51,24 +53,76 @@ class QA(GenericTask):
         util.createScript('metho.html', htmlCode)
 
 
+    def __configGet(self, section, key):
+        try:
+            value = self.config.get(section, key)
+        except:
+            value = None
+        return value
+
+
     def __getTags(self):
         tags = {}
 
+        # ----------------------------------------------------
+        # PREPARATION SECTION
+        # ----------------------------------------------------
         denoisingKeys = ['number_array_coil']
         for key in denoisingKeys:
-            tags[key] = self.config.get('denoising', key)
+            tags[key] = self.__configGet('denoising', key)
 
         methodologyKeys = [
+                'manufacturer', 'magneticfieldstrenght', 'mrmodel',
                 't1_tr', 't1_te', 't1_ti', 't1_flipangle', 't1_fov', 't1_mat',
-                't1_voxelsize', 'dwi_tr', 'dwi_te', 'dwiflipangle',
+                't1_slices', 't1_voxelsize', 'dwi_tr', 'dwi_te', 'dwiflipangle',
                 'dwi_voxelsize', 'dwi_numdirections', 'dwi_bvalue']
-        for key in denoisingKeys:
-            tags[key] = self.config.get('methodology', key)
+        for key in methodologyKeys:
+            tags[key] = self.__configGet('methodology', key)
 
-        """
-        , 'correction',
-'fsl_vers', 'fsl_ref', 'denoising', 'tri_ref', 'fdt_ref', 'seg_ref', 'references']
-        """
+        # ----------------------------------------------------
+        # PREPROCESSING
+        # ----------------------------------------------------
+        # Add the FSL references
+        refs = [self.__configGet('references', 'fsl')]
+        tags['fsl_vers']= None # parser.get('', '')
+        tags['fsl_ref'] = "[{}]".format(len(refs))
+
+        # Prepare the text for the denoising section
+        if self.config.get('denoising', 'ignore') in ['True', 'true']:
+            tags['denoising'] = False
+        else:
+            tags['denoising'] = True
+            method = self.config.get('denoising', 'algorithm')
+            tags['algorithm'] = method
+            refs.append(self.__configGet('references', method))
+            tags['denoising_ref'] = len(refs)
+
+        # Prepare the text for the correction section
+        if self.config.get('correction', 'ignore') in ['True', 'true']:
+            tags['correction'] = False
+        else:
+            tags['correction'] = True
+            tags['correction_method'] = None
+
+        # Add the trilinear interpolation references
+        refs.append(self.__configGet('references', 'tri1'))
+        refs.append(self.__configGet('references', 'tri2'))
+        tags['tri_ref']= "[{}, {}]".format(len(refs)-1, len(refs))
+
+        # Add the FDT reference
+        refs.append(self.__configGet('references', 'fdt'))
+        tags['fdt_ref']= "[{}]".format(len(refs))
+
+        # Add Freesurfer segmentation/label references
+        refs.append(self.__configGet('references', 'segfreesurfer'))
+        refs.append(self.__configGet('references', 'labfreesurfer'))
+        tags['seg_ref']= "[{}, {}]".format(len(refs)-1, len(refs))
+
+        refTxt = []
+        for idx, ref in enumerate(refs):
+            refTxt.append("<p>[{}] {}</p></ br>".format(idx+1, ref))
+        tags['references']= refTxt
+
         return tags
 
 
--- templates/files/metho.tpl
+++ templates/files/metho.tpl
@@ -1,27 +1,35 @@
 <h3>Acquisition</h3>
 
 <p>T1 and diffusion weighed images (DWI) were acquired with
-a {{number_array_coil}} -channel head coil and a 3.0T Siemens TIM TRIO magnetic resonance
+a {{number_array_coil}}-channel head coil and a {{magneticfieldstrenght}}T {{manufacturer}} {{mrmodel}} magnetic resonance
 imaging system (T1: TR= {{t1_tr}} ms, TE = {{t1_te}} ms, TI = {{t1_ti}} ms, Flip Angle =
-{{t1_flipangle}} &deg;, 176 slices, FoV = {{t1_fov}} mm<sup>2</sup>, matrix size = {{t1_mat}}, voxel size = {{t1_voxelsize}}
+{{t1_flipangle}} &deg;, {{t1_slices}} slices, FoV = {{t1_fov}} mm<sup>2</sup>, matrix size = {{t1_mat}}, voxel size = {{t1_voxelsize}}
  mm<sup>3</sup>; DWI: TR = {{dwi_tr}} ms, TE = {{dwi_te}} ms, Flip Angle = {{dwiflipangle}} &deg;, voxel size:
 {{dwi_voxelsize}} mm<sup>3</sup>, no gap). The DWI corresponded to a high angular resolution
 diffusion MRI (HARDI) acquisition using spin echo EPI diffusion-encoded
 images was performed along {{dwi_numdirections}} independent directions, with b-value of {{dwi_bvalue}}
 s/mm<sup>2</sup>.</p>
 
-{{correction}}
-
 <h3>Prepocessing</h3>
+
+{% if denoising %}
+<p>Denoising of DWI was done using {{algorithm}} method [{{denoising_ref}}].</p>
+{% else %}
+<p>DWI images were not denoised.</p>
+{% endif %}
+
+{% if correction %}
+<p>DWI images were corrected using the {{correction_method}} method.</p>
+{% else %}
+<p>DWI images were not corrected.</p>
+{% endif %}
 
 <p>The FMRIB Diffusion toolbox EDDY of FSL {{fsl_vers}} {{fsl_ref}} was used to correct all images for subject movement and eddy-currents. Gradient directions were corrected corresponding to motion correction parameters.</p>
 
-{{denoising}}
+<p>DWI were upsampled to 1mm isotropic resolution using a trilinear interpolation {{tri_ref}}. Eigenvector, eigenvalues, fractional anisotropy (FA) were finally extracted from tensor model using FDT {{fdt_ref}} toolbox from FSL.
+T1 image went through Freesurfer's pipeline {{seg_ref}} to get a segmentation of the corpus callosum. T1-weighted image was registered to the DWI using FSL.</p>
 
-<p>DWI were upsampled to 1mm isotropic resolution using a trilinear interpolation {{tri_ref}}. Eigenvector, eigenvalues, fractional anisotropy (FA) were finally extracted from tensor model using FDT {{fdt_ref}} toolbox from FSL. 
-T1 image went through Freesurfer’s pipeline {{seg_ref}} to get a segmentation of the corpus callosum. T1-weighted image was registered to the DWI using FSL.</p>
-
-<p>These tools were wrapped in the TOAD pipeline developed in the Functional Neuroimaging Unit (UNF) from the Centre de Recherche de l’Institut Universitaire de Gériatrie de Montréal (<a href='http://www.unf-montreal.ca/toad'>http://www.unf-montreal.ca/toad</a></p>
+<p>These tools were wrapped in the TOAD pipeline developed in the Functional Neuroimaging Unit (UNF) from the Centre de Recherche de l'Institut Universitaire de Geriatrie de Montreal (<a href='http://www.unf-montreal.ca/toad'>http://www.unf-montreal.ca/toad</a>)</p>
 
 <h3>References</h3>
 
